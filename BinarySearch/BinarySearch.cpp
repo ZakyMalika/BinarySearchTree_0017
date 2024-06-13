1bdbdfaee523f3af105286093ebfb0eeec97d655
ZakyMalika <zakymalika0987@gmail.com>
2024-06-13 18:29:45 +0700

    membuat bagian kiri dari neew node menjadi null

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -29,6 +29,9 @@ public:
 
 	void insert(string element) // insert a node in the binary search
 	{
-		Node* newNode = new Node(element, nullptr, nullptr); // 
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for thhe new Node
+		newNode->info = element;	//assign value to the data field of the new data
+		newNode->leftchild = nullptr; // make the left child of the new node point to null
+		newNode->rightchild = nullptr;
 	}
 };
